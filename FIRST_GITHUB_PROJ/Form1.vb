e8c0e8390597822ab0ff72c1f480fcc321f3d41b
Sangeetha T <sangeethatmsc@outlook.com>
2023-11-03 12:27:02 +0530

    changes4

--- FIRST_GITHUB_PROJ/Form1.vb
+++ FIRST_GITHUB_PROJ/Form1.vb
@@ -2,5 +2,8 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim I As Integer
         Dim s As String
+        Dim ss As String
+        '        xx, components,,
+        'mjg
     End Sub
 End Class
